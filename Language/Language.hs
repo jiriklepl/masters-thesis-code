20761e64e7639ce948839844d4cef7aad0722e96
jiriklepl <jiriklepl@seznam.cz>
2021-03-22 04:51:01 +0100

    modified grammar

--- Language/Language.hs
+++ Language/Language.hs
@@ -17,15 +17,22 @@ data Decl a = ImportDecl [Import a] a -- at least one
             | ExportDecl [Export a] a -- at least one
             | ConstDecl (Maybe (Type a)) (Name a) (Expr a) a
             | TypedefDecl (Type a) [Name a] a -- at least one
-            | RegDecl IsInvariant (Registers a) a
+            | RegDecl (Maybe Invariant) (Registers a) a
             | PragmaDecl (Name a) (Pragma a) a
--- | target {memsize int | byteorder (little | big) | pointersize int | wordsize int};
+            | TargetDecl [TargetDirective a] a
+
+data TargetDirective a = MemSize Int a
+                       | BiteOrder Endian a
+                       | PointerSize Int a
+                       | WordSize Int a
+
+data Endian = Little | Big -- TODO: or just Bool?
 
 data Import a = Import (Maybe String) (Name a) a
 
 data Export a = Export (Name a) (Maybe String) a
 
-newtype IsInvariant = Invariant Bool -- TODO: or it can be just Bool?
+data Invariant = Invariant -- TODO: or it can be just Bool?
 
 data Datum a = DatumLabel (Name a) a
              | DatumAlign Int a
@@ -47,11 +54,13 @@ data BodyItem a = BodyDecl (Decl a) a
 
 data Procedure a = Procedure (Maybe Conv) (Name a) [Formal a] (Body a) a
 
-data Formal a = Formal (Maybe Kind) IsInvariant (Type a) (Name a) a
+data Formal a = Formal (Maybe Kind) (Maybe Invariant) (Type a) (Name a) a
 
 data Actual a = Actual (Maybe Kind) (Expr a) a
 
 newtype Kind = Kind String -- can be IEEE 754, integer, floating point, qubite, oracle byte, etc.
+
+data KindName a = KindName (Maybe Kind) (Name a) a
 
 data StackDecl a = StackDecl [Datum a] a -- at least one
 
@@ -60,12 +69,12 @@ data Stmt a = EmptyStmt a
             | SwitchStmt (Expr a) [Arm a] a
             | SpanStmt (Expr a) (Expr a) (Body a) a
             | AssignStmt [LValue a] [Expr a] a -- TODO: maybe list of tuples?
-            | CallStmt [LValue a] (Name a) [Flow a] a
-            -- TODO: [kindednames =] [conv] expr ( [actuals] ) [targets] {flow | alias};
+            | PrimOpStmt [LValue a] (Name a) [Flow a] a
+            | CallStmt [KindName a] (Maybe Conv) (Expr a) [Actual a] (Maybe (Targets a)) [Either (Flow a) (Alias a)] a
             | JumpStmt (Maybe Conv) (Expr a)  [Actual a] (Maybe (Targets a)) a
-            -- TODO: | ReturnStmt
+            | ReturnStmt (Maybe Conv) (Maybe (Expr a, Expr a)) [Actual a] a
             | LabelStmt (Name a) a
-            | ContStmt (Name a) [(Kind, Name a)] a
+            | ContStmt (Name a) [KindName a] a
             | GotoStmt (Expr a) (Maybe (Targets a)) a
             | CutToStmt (Expr a) [Actual a] [Flow a] a
 
@@ -76,16 +85,24 @@ data Range a = Range (Expr a) (Maybe (Expr a)) a
 data LValue a = LVName (Name a) a -- TODO: name
               | LVRef (Type a) (Expr a) (Maybe (Asserts a)) a -- TODO: name
 
-data Flow a -- TODO: rewrite this
+data Flow a = AlsoCutsTo [Name a] a -- at least one
+            | AlsoUnwindsTo [Name a] a -- at least one
+            | AlsoReturnsTo [Name a] a -- at least one
+            | AlsoAbsorbs a
+            | NeverReturns a
 
 data Alias a = Reads [Name a] a | Writes [Name a] a
 
 data Targets a = Targets [Name a] a
 
 data Expr a = LitExpr (Lit a) (Maybe (Type a)) a
-            | LVExpr (LValue a) a
-            | ParExpr (Expr a) a -- TODO: do we even need this?
-            | BinExpr (Expr a) (Op a) (Expr a) a -- TODO: does op even need a parameter?
+            | NameExpr (Name a) a
+            | RefExpr (Type a) (Expr a) (Maybe (Asserts a)) a
+            | ParExpr (Expr a) a
+            | BinOpExpr (Expr a) Op (Expr a) a
+            | ComExpr (Expr a) a
+            | NegExpr (Expr a) a
+            | InfixExpr (Expr a) (Name a) (Expr a) a
             | PrefixExpr (Name a) [Actual a] a -- TODO: name
 
 data Lit a = LitInt Int a -- TODO: think more
@@ -100,9 +117,8 @@ newtype String16 = String16 String -- TODO: implement String16
 newtype Conv = Foreign String
 
 data Asserts a = AlignAssert Int [Name a] a
-               -- TODO: and the reverse of this?
 
-data Op a = OpName (Name a) | OpAdd | OpSub | OpMul | OpDiv | OpMod | OpAnd | OpOr | OpNeg | OpShL | OpShR | OpEq | OpNeq | OpGT | OpLT | OpGE | OpLE
+data Op = AddOp | SubOp | MulOp | DivOp | ModOp | AndOp | OrOp | NorOp | ShLOp | ShROp | EqOp | NeqOp | GtOp | LtOp | GeOp | LeOp
 
 data Name a = Name String a -- TODO: consult this with the manual (and mirek) (and also add hash etc?)
 
