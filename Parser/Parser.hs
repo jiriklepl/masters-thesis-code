0c9375a90fe27fd25452f985eb02471a8c57e25d
jiriklepl <jiriklepl@seznam.cz>
2021-03-24 07:42:50 +0100

    added initial dummy implementation

--- Parser/Parser.hs
+++ Parser/Parser.hs
@@ -1,6 +1,25 @@
 module Parser (someFunc) where
 
 import Text.Megaparsec
+import Text.Megaparsec.Char
+import Data.Void
+import qualified Data.Text as T
+import qualified Control.Monad.State as State
+import Language
+
+data ParserState = ParserState
+    { dummy1 :: [String]
+    , dummy2 :: [String]
+    }
+
+type StateMonad = State ParserState
+type Parser a = ParsecT Void T.Text StateMonad (a Int)
+
+fib a = 10 :: Int
+
+-- | Parses the whole 'Unit'
+unit :: Parser Unit
+unit = undefined
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
