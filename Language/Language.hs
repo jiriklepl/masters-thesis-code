a11a3e6dea6369227abde3cfa868b31075281115
jiriklepl <jiriklepl@seznam.cz>
2021-04-29 03:20:10 +0200

    added more parser

--- Language/Language.hs
+++ Language/Language.hs
@@ -1,24 +1,27 @@
 module Language where
 
+import qualified Data.Text as T
+import Data.Text (Text)
+
 -- TODO: categorize names? (different namespaces for variables, operators, etc)
 
 data Unit a = Unit [TopLevel a] a
 
-data TopLevel a = TopSection String [Section a] a
-                | TopDecl (Decl a) a
-                | TopProcedure (Procedure a) a
+data TopLevel a = TopSection Text [Section a] a
+                | TopDecl (Decl a)
+                | TopProcedure (Procedure a)
 
-data Section a = SecDecl (Decl a) a
-               | SecProcedure (Procedure a) a
-               | SecDatum (Datum a) a
+data Section a = SecDecl (Decl a)
+               | SecProcedure (Procedure a)
+               | SecDatum (Datum a)
                | SecSpan (Expr a) (Expr a) [Section a] a
 
 data Decl a = ImportDecl [Import a] a -- at least one
             | ExportDecl [Export a] a -- at least one
-            | ConstDecl (Maybe (Type a)) (Name a) (Expr a) a
-            | TypedefDecl (Type a) [Name a] a -- at least one
+            | ConstDecl (Maybe (Type a)) Name (Expr a) a
+            | TypedefDecl (Type a) [Name] a -- at least one
             | RegDecl (Maybe Invariant) (Registers a) a
-            | PragmaDecl (Name a) (Pragma a) a
+            | PragmaDecl Name (Pragma a) a
             | TargetDecl [TargetDirective a] a
 
 data TargetDirective a = MemSize Int a
@@ -28,21 +31,21 @@ data TargetDirective a = MemSize Int a
 
 data Endian = Little | Big -- TODO: or just Bool?
 
-data Import a = Import (Maybe String) (Name a) a
+data Import a = Import (Maybe Text) Name a
 
-data Export a = Export (Name a) (Maybe String) a
+data Export a = Export Name (Maybe Text) a
 
 data Invariant = Invariant -- TODO: or it can be just Bool?
 
-data Datum a = DatumLabel (Name a) a
+data Datum a = DatumLabel Name a
              | DatumAlign Int a
              | Datum (Type a) (Maybe (Size a)) (Maybe (Init a)) a
 
 data Init a = ExprInit [Expr a] a
-            | StrInit String a
+            | StrInit Text a
             | Str16Init String16 a
 
-data Registers a = Registers (Maybe Kind) (Type a) [(Name a, Maybe String)] a -- at least one
+data Registers a = Registers (Maybe Kind) (Type a) [(Name, Maybe Text)] a -- at least one
 
 data Size a = Size (Maybe (Expr a)) a
 
@@ -52,16 +55,16 @@ data BodyItem a = BodyDecl (Decl a) a
                 | BodyStackDecl (StackDecl a) a
                 | BodyStmt (Stmt a) a
 
-data Procedure a = Procedure (Maybe Conv) (Name a) [Formal a] (Body a) a
+data Procedure a = Procedure (Maybe Conv) Name [Formal a] (Body a) a
 
-data Formal a = Formal (Maybe Kind) (Maybe Invariant) (Type a) (Name a) a
+data Formal a = Formal (Maybe Kind) (Maybe Invariant) (Type a) Name a
 
 data Actual a = Actual (Maybe Kind) (Expr a) a
 
-newtype Kind = Kind String -- can be IEEE 754, integer, floating point, qubite, oracle byte, etc.
+newtype Kind = Kind Text -- can be IEEE 754, integer, floating point, qubite, oracle byte, etc.
 -- TODO: should be a part of inference?
 
-data KindName a = KindName (Maybe Kind) (Name a) a
+data KindName a = KindName (Maybe Kind) Name a
 
 data StackDecl a = StackDecl [Datum a] a -- at least one
 
@@ -70,12 +73,12 @@ data Stmt a = EmptyStmt a
             | SwitchStmt (Expr a) [Arm a] a
             | SpanStmt (Expr a) (Expr a) (Body a) a
             | AssignStmt [LValue a] [Expr a] a -- TODO?: maybe list of tuples
-            | PrimOpStmt [LValue a] (Name a) [Flow a] a
+            | PrimOpStmt [LValue a] Name [Flow a] a
             | CallStmt [KindName a] (Maybe Conv) (Expr a) [Actual a] (Maybe (Targets a)) [Either (Flow a) (Alias a)] a
             | JumpStmt (Maybe Conv) (Expr a)  [Actual a] (Maybe (Targets a)) a
             | ReturnStmt (Maybe Conv) (Maybe (Expr a, Expr a)) [Actual a] a
-            | LabelStmt (Name a) a
-            | ContStmt (Name a) [KindName a] a
+            | LabelStmt Name a
+            | ContStmt Name [KindName a] a
             | GotoStmt (Expr a) (Maybe (Targets a)) a
             | CutToStmt (Expr a) [Actual a] [Flow a] a
 
@@ -83,45 +86,45 @@ data Arm a = Arm [Range a] (Body a) a
 
 data Range a = Range (Expr a) (Maybe (Expr a)) a
 
-data LValue a = LVName (Name a) a -- TODO?: name
+data LValue a = LVName Name a -- TODO?: name
               | LVRef (Type a) (Expr a) (Maybe (Asserts a)) a -- TODO?: name
 
-data Flow a = AlsoCutsTo [Name a] a -- at least one
-            | AlsoUnwindsTo [Name a] a -- at least one
-            | AlsoReturnsTo [Name a] a -- at least one
+data Flow a = AlsoCutsTo [Name] a -- at least one
+            | AlsoUnwindsTo [Name] a -- at least one
+            | AlsoReturnsTo [Name] a -- at least one
             | AlsoAbsorbs a
             | NeverReturns a
 -- TODO: should be a part of inference?
 
-data Alias a = Reads [Name a] a | Writes [Name a] a
+data Alias a = Reads [Name] a | Writes [Name] a
 
-data Targets a = Targets [Name a] a
+data Targets a = Targets [Name] a
 
 data Expr a = LitExpr (Lit a) (Maybe (Type a)) a
-            | NameExpr (Name a) a
+            | NameExpr Name a
             | RefExpr (Type a) (Expr a) (Maybe (Asserts a)) a
             | ParExpr (Expr a) a
             | BinOpExpr (Expr a) Op (Expr a) a
             | ComExpr (Expr a) a
             | NegExpr (Expr a) a
-            | InfixExpr (Expr a) (Name a) (Expr a) a
-            | PrefixExpr (Name a) [Actual a] a
+            | InfixExpr (Expr a) Name (Expr a) a
+            | PrefixExpr Name [Actual a] a
 
 data Lit a = LitInt Int a -- TODO?: think more
            | LitFloat Float a
            | LitChar Char a
 
 data Type a = TBits Int a -- TODO?: name
-            | TName (Name a) a -- TODO?: name
+            | TName (Name) a -- TODO?: name
 
-newtype String16 = String16 String -- TODO: implement String16
+newtype String16 = String16 Text -- TODO: implement String16
 
-newtype Conv = Foreign String
+newtype Conv = Foreign Text
 
-data Asserts a = AlignAssert Int [Name a] a
+data Asserts a = AlignAssert Int [Name] a
 
 data Op = AddOp | SubOp | MulOp | DivOp | ModOp | AndOp | OrOp | NorOp | ShLOp | ShROp | EqOp | NeqOp | GtOp | LtOp | GeOp | LeOp
 
-data Name a = Name String a -- TODO: consult this with the manual (and mirek) (and also add hash etc?)
+newtype Name = Name Text -- TODO: consult this with the manual (and mirek) (and also add hash etc?)
 
 data Pragma a -- TODO: the manual doesn't specify at all
--- Parser/Parser.hs
+++ Parser/Parser.hs
@@ -4,7 +4,9 @@ import Text.Megaparsec
 import Text.Megaparsec.Char
 import Data.Void
 import qualified Data.Text as T
+import Data.Text (Text)
 import qualified Control.Monad.State as State
+import qualified Text.Megaparsec.Char.Lexer as L
 import Language
 
 data ParserState = ParserState
@@ -13,13 +15,96 @@ data ParserState = ParserState
     }
 
 type StateMonad = State ParserState
-type Parser a = ParsecT Void T.Text StateMonad (a Int)
+type Parser a = ParsecT Void Text StateMonad a
 
-fib a = 10 :: Int
+sc :: Parser ()
+sc = L.space space1 (L.skipLineComment (T.pack "//")) (L.skipBlockComment (T.pack "/*") (T.pack "*/"))
+
+lexeme :: Parser a -> Parser a
+lexeme = L.lexeme sc
+
+symbol :: Text -> Parser Text
+symbol = L.symbol sc
+
+keyword :: Text -> Parser Text
+keyword = lexeme . string
+
+stringLiteral :: Parser Text
+stringLiteral = char '"' >> T.pack <$> manyTill L.charLiteral (char '"')
+
+name :: Parser Text
+name = do
+    n <- letterChar <|> oneOf "_.$@"
+    ame <- T.pack <$> many (alphaNumChar <|> oneOf "_.$@")
+    return $ n `T.cons` ame
+
+braces :: Parser a -> Parser a
+braces = between (symbol (T.pack "{")) (symbol (T.pack "}"))
 
 -- | Parses the whole 'Unit'
-unit :: Parser Unit
-unit = undefined
+unit :: Parser (Unit SourcePos)
+unit = do
+    pos <- getSourcePos
+    topLevels <- many topLevel
+    return $ Unit topLevels pos
+
+topLevel :: Parser (TopLevel SourcePos)
+topLevel = sectionTopLevel <|> TopDecl <$> decl <|> TopProcedure <$> procedure
+
+sectionTopLevel = do
+    keyword (T.pack "section")
+    pos <- getSourcePos
+    name <- stringLiteral
+    sections <- braces $ many section
+    return $ TopSection name sections pos
+
+section :: Parser (Section SourcePos)
+section = SecDecl <$> decl <|> secSpan <|> try (SecProcedure <$> procedure) <|> SecDatum <$> datum
+
+decl :: Parser (Decl SourcePos)
+decl = importDecl <|> exportDecl <|> constDecl <|> typedefDecl <|> registerDecl <|> pragmaDecl <|> targetDecl
+
+importDecl :: Parser (Decl SourcePos)
+importDecl = do
+    keyword (T.pack "import")
+    pos <- getSourcePos
+    imports <- sepBy1 import_ (symbol (T.pack ","))
+    return $ ImportDecl imports pos
+
+exportDecl :: Parser (Decl SourcePos)
+exportDecl = do
+    keyword (T.pack "export")
+    pos <- getSourcePos
+    exports <- sepBy1 export (symbol (T.pack ","))
+    return $ ExportDecl exports pos
+
+constDecl :: Parser (Decl SourcePos)
+constDecl = do
+    keyword (T.pack "const")
+    pos <- getSourcePos
+    try (do
+        t <- type_
+        declName <- Name <$> name
+        expr <- expression
+        return $ ConstDecl (Just t) declName expr pos)
+      <|> do
+        declName <- Name <$> name
+        expr <- expression
+        return $ ConstDecl Nothing declName expr pos
+
+procedure = undefined -- TODO
+secSpan = undefined -- TODO
+datum = undefined -- TODO
+import_ = undefined -- TODO
+export = undefined -- TODO
+
+typedefDecl = undefined -- TODO
+registerDecl = undefined -- TODO
+pragmaDecl = undefined -- TODO
+targetDecl = undefined -- TODO
+type_ = undefined -- TODO
+expression = undefined -- TODO
+
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
