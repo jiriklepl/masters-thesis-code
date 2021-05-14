511e9567935a3fcd9723c2ae23edf3d999824fa4
jiriklepl <jiriklepl@seznam.cz>
2021-05-14 06:47:09 +0200

    running the parser

--- Compiler/Main.hs
+++ Compiler/Main.hs
@@ -1,4 +1,13 @@
 module Main where
 
+import Text.Megaparsec
+import Data.Text as T
+import Data.Text.IO as T
+import Control.Monad.State as State
+
+import Parser
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+    contents <- T.getContents
+    print . flip State.runState initState $ runParserT unit "stdin" contents
