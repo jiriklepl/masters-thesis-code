46d9f9c2e2af838628bb791959f661e9b0b686cf
jiriklepl <jiriklepl@seznam.cz>
2021-10-03 07:21:49 +0200

    fixed spelling

--- test/QuasiQuotes.hs
+++ test/QuasiQuotes.hs
@@ -8,14 +8,14 @@ import Language.Haskell.TH.Quote
 text :: QuasiQuoter
 text =
   QuasiQuoter
-    { quoteExp = return . LitE . StringL . normaliseNewlines
+    { quoteExp = return . LitE . StringL . normalizeNewlines
     , quotePat = const $ fail "pattern not implemented"
     , quoteType = const $ fail "type not implemented"
     , quoteDec = const $ fail "declaration not implemented"
     }
 
-normaliseNewlines :: String -> String
-normaliseNewlines [] = []
-normaliseNewlines ('\r':'\n':cs) = '\n' : normaliseNewlines cs
-normaliseNewlines ('\r':cs) = '\n' : normaliseNewlines cs
-normaliseNewlines (c:cs) = c : normaliseNewlines cs
+normalizeNewlines :: String -> String
+normalizeNewlines [] = []
+normalizeNewlines ('\r':'\n':cs) = '\n' : normalizeNewlines cs
+normalizeNewlines ('\r':cs) = '\n' : normalizeNewlines cs
+normalizeNewlines (c:cs) = c : normalizeNewlines cs
