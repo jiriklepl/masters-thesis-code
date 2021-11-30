cef3c24dd27da02d4740c936354a08125aeb5b9b
jiriklepl <jiriklepl@seznam.cz>
2021-11-30 08:09:57 +0100

    refactored Variables.hs

--- src/CMM/AST/Variables.hs
+++ src/CMM/AST/Variables.hs
@@ -1,87 +1,24 @@
-{-# LANGUAGE Trustworthy #-}
-{-# LANGUAGE TemplateHaskell #-}
-{-# LANGUAGE ConstraintKinds #-}
+{-# LANGUAGE Safe #-}
 {-# LANGUAGE FlexibleContexts #-}
-{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE Rank2Types #-}
 
 module CMM.AST.Variables where
 
-import Control.Lens.Getter
-import Control.Lens.Setter
-import Control.Lens.TH (makeLenses)
-import Control.Monad.State
-import Data.Data
-import Data.Foldable
-import Data.Generics.Aliases
-import Data.Set (Set)
-import qualified Data.Set as Set
-import Data.Text (Text)
-import Prettyprinter
+import safe Control.Lens.Getter
+import safe Control.Monad.State
+import safe Data.Data
+import safe Data.Foldable
+import safe Data.Generics.Aliases
+import safe Data.Set (Set)
+import safe Data.Text (Text)
 
-import CMM.AST
-import CMM.AST.Annot
-import CMM.AST.HasName
-import CMM.Parser.HasPos
-import CMM.Pretty ()
-import CMM.Warnings
-
-data CollectedVariables =
-  CollectedVariables
-    { _variables :: Set Text
-    , _typeVariables :: Set Text
-    , _errors :: Int
-    , _warnings :: Int
-    }
-
-initCollectedVariables :: CollectedVariables
-initCollectedVariables =
-  CollectedVariables
-    {_variables = mempty, _typeVariables = mempty, _errors = 0, _warnings = 0}
-
-type MonadCollectVariables m = (MonadState CollectedVariables m, MonadIO m)
-
-makeLenses ''CollectedVariables
-
-registerError ::
-     (HasPos n, Pretty n, MonadCollectVariables m) => n -> Text -> m ()
-registerError node message = do
-  errors += 1
-  makeMessage mkError node message
-
-registerWarning ::
-     (HasPos n, Pretty n, MonadCollectVariables m) => n -> Text -> m ()
-registerWarning node message = do
-  warnings += 1
-  makeMessage mkWarning node message
-
-addVar :: (HasPos n, Pretty n, MonadCollectVariables m) => n -> Text -> m ()
-addVar node var = do
-  uses variables (var `Set.member`) >>= \case
-    True -> registerError node "Duplicate variable"
-    False -> variables %= Set.insert var
-
-addVarTrivial ::
-     (HasPos n, Pretty n, HasName n, MonadCollectVariables m) => n -> m n
-addVarTrivial n = n <$ addVar n (getName n)
-
-addTVar :: (HasPos n, Pretty n, MonadCollectVariables m) => n -> Text -> m ()
-addTVar node tVar = do
-  uses variables (tVar `Set.member`) >>= \case
-    True -> registerError node "Duplicate type variable"
-    False -> variables %= Set.insert tVar
-
-addTVarTrivial ::
-     (HasPos n, Pretty n, HasName n, MonadCollectVariables m) => n -> m n
-addTVarTrivial n = n <$ addTVar n (getName n)
-
-variablesCommon ::
-     MonadIO m => StateT CollectedVariables m a -> m (Set Text, Set Text)
-variablesCommon go = do
-  result <- execStateT go initCollectedVariables
-  return (result ^. variables, result ^. typeVariables)
+import safe CMM.AST
+import safe CMM.AST.Annot
+import safe CMM.AST.HasName
+import safe CMM.AST.Variables.State
+import safe CMM.Parser.HasPos
 
 localVariables :: (MonadIO m, HasPos a) => Procedure a -> m (Set Text, Set Text)
 localVariables n = variablesCommon . go $ getPos <$> n
@@ -94,6 +31,12 @@ globalVariables n = variablesCommon . go $ getPos <$> n
   where
     go :: (Data d, MonadCollectVariables m) => d -> m d
     go = addGlobalCases $ addCommonCases $ gmapM go
+
+variablesCommon ::
+     MonadIO m => StateT CollectedVariables m a -> m (Set Text, Set Text)
+variablesCommon go = do
+  result <- execStateT go initCollectedVariables
+  return (result ^. variables, result ^. typeVariables)
 
 infixr 3 $|
 
---
+++ src/CMM/AST/Variables/State.hs
@@ -0,0 +1,73 @@
+{-# LANGUAGE Trustworthy #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ConstraintKinds #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE Rank2Types #-}
+
+module CMM.AST.Variables.State where
+
+import safe Control.Lens.TH (makeLenses)
+import safe Control.Monad.State ( MonadState, MonadIO )
+import safe Data.Set (Set)
+import safe qualified Data.Set as Set
+import safe Data.Text (Text)
+import safe Prettyprinter ( Pretty )
+
+
+import safe Control.Lens.Getter ( uses )
+import safe Control.Lens.Setter ( (%=), (+=) )
+import safe CMM.AST.HasName ( HasName(..) )
+import safe CMM.Parser.HasPos ( HasPos )
+import safe CMM.Pretty ()
+import safe CMM.Warnings ( makeMessage, mkError, mkWarning )
+
+data CollectedVariables =
+  CollectedVariables
+    { _variables :: Set Text
+    , _typeVariables :: Set Text
+    , _errors :: Int
+    , _warnings :: Int
+    }
+
+initCollectedVariables :: CollectedVariables
+initCollectedVariables =
+  CollectedVariables
+    {_variables = mempty, _typeVariables = mempty, _errors = 0, _warnings = 0}
+
+type MonadCollectVariables m = (MonadState CollectedVariables m, MonadIO m)
+
+makeLenses ''CollectedVariables
+
+registerError ::
+     (HasPos n, Pretty n, MonadCollectVariables m) => n -> Text -> m ()
+registerError node message = do
+  errors += 1
+  makeMessage mkError node message
+
+registerWarning ::
+     (HasPos n, Pretty n, MonadCollectVariables m) => n -> Text -> m ()
+registerWarning node message = do
+  warnings += 1
+  makeMessage mkWarning node message
+
+addVar :: (HasPos n, Pretty n, MonadCollectVariables m) => n -> Text -> m ()
+addVar node var = do
+  uses variables (var `Set.member`) >>= \case
+    True -> registerError node "Duplicate variable"
+    False -> variables %= Set.insert var
+
+addVarTrivial ::
+     (HasPos n, Pretty n, HasName n, MonadCollectVariables m) => n -> m n
+addVarTrivial n = n <$ addVar n (getName n)
+
+addTVar :: (HasPos n, Pretty n, MonadCollectVariables m) => n -> Text -> m ()
+addTVar node tVar = do
+  uses variables (tVar `Set.member`) >>= \case
+    True -> registerError node "Duplicate type variable"
+    False -> variables %= Set.insert tVar
+
+addTVarTrivial ::
+     (HasPos n, Pretty n, HasName n, MonadCollectVariables m) => n -> m n
+addTVarTrivial n = n <$ addTVar n (getName n)
