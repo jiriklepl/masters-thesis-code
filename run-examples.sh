527e614a76876f7ffe39944e552a03d55b751faf
jiriklepl <jiriklepl@seznam.cz>
2022-06-14 19:13:15 +0200

    run-examples script exits if it fails to build

--- run-examples.sh
+++ run-examples.sh
@@ -1,9 +1,8 @@
-
 #!/bin/sh
 
 mkdir -p examples/out
 
-cabal build Compiler
+cabal build Compiler || exit 1
 
 for file in $(find examples -maxdepth 1 -mindepth 1 -name "*.chmmm"); do
     cabal run -v0 Compiler < ${file} > examples/out/$(basename ${file})
