b08fa5d2b54037215e04058b4acf2652c6b15ed2
jiriklepl <jiriklepl@seznam.cz>
2022-06-17 15:21:21 +0200

    added church

---
+++ src/CMM/Data/Church.hs
@@ -0,0 +1,12 @@
+{-# LANGUAGE Safe #-}
+
+module CMM.Data.Church where
+
+import safe Data.Function ( id, (.) )
+import safe Data.Eq ( Eq )
+
+import safe CMM.Data.Num ( Num((-)) )
+
+church :: (Num n, Eq n) => n -> (a -> a) -> a -> a
+church 0 _ = id
+church n f = f . church (n - 1) f
