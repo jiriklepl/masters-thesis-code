b66fc03f0b41adc51f00f288415c7e5f537d8b31
jiriklepl <jiriklepl@seznam.cz>
2022-07-04 09:24:58 +0200

    improved, more general, runner script

--- run-examples.sh
+++ run-examples.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+which cabal 2> /dev/null 1> /dev/null
+
+[ $? -ne 0 ] && echo "Please install cabal" && exit 1
+
+USE_LLC=$(which llc-12 || which llc12 || which llc-13 || which llc13 || which llc-14 || llc14 || which llc) 2> /dev/null
+
+[ "$USE_LLC" = "" ] && echo "Please install llc (preferably version 12)" && exit 1
 
 run_tests() {
 
@@ -7,6 +14,7 @@ find examples -maxdepth 1 -mindepth 1 -name "*.chmmm" | {
     TOTAL=0
     SUCCESS=0
     FAILURE=0
+    FAILED=""
 
     while read -r file; do
         TOTAL=$((TOTAL+1))
@@ -22,20 +30,35 @@ find examples -maxdepth 1 -mindepth 1 -name "*.chmmm" | {
 
         echo "doing $file" > /dev/stderr
 
-        if cabal run -v0 Compiler -- -o "$out" "$file" && llc-12 -o /dev/null "$out"; then
+        if cabal run -v0 Compiler -- -o "$out" "$file" && "$USE_LLC" -o /dev/null "$out"; then
             RESULT_C=1
         else
             RESULT_C=0
         fi
+
         SUCCESS=$(( SUCCESS + FAILURE_C * RESULT_C + (1 - FAILURE_C) * (1 - RESULT_C) ))
+        OLD_FAILURE="$FAILURE"
         FAILURE=$(( FAILURE + (1 - FAILURE_C) * RESULT_C + (FAILURE_C) * (1 - RESULT_C) ))
+
+        if [ "$OLD_FAILURE" -lt "$FAILURE" ]; then
+            FAILED="$file, $FAILED"
+        fi
     done
 
-    echo "SUCCESS: $SUCCESS"
-    echo "FAILURE: $FAILURE"
+    echo ""
+    echo ""
+    echo ""
+
+    echo "SUCCESS: $SUCCESS (including expected exits on wrong inputs)"
+    echo "FAILURE: $FAILURE (including unexpected exits)"
     echo "TOTAL: $TOTAL"
 
-    [ "$SUCCESS" -lt "$TOTAL" ] && exit 2
+    if [ "$SUCCESS" -lt "$TOTAL" ]; then
+        echo "FAILED:"
+        echo "$FAILED"
+
+        exit 2
+    fi
 }
 
 }
